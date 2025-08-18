10eb911527e1acea94d6cc3e005cb380d802cf6c
KavyaGundu <149193745+GunduKavya@users.noreply.github.com>
2025-08-18 20:55:03 +0530

    33984

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import homepage_view
+from . import views
 
 urlpatterns = [
     path('', homepage_view, name='homepage'),
+    path('contact/',views.contact_view, name='contact'),
 ]
