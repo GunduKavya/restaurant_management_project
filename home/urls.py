72ecd07d67d60a9fa35a5827bcb26400c71662c2
KavyaGundu <149193745+GunduKavya@users.noreply.github.com>
2025-08-20 14:34:55 +0530

    35430

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', homepage_view, name='homepage'),
     path('contact/',views.contact_view, name='contact'),
+    path('reservations/', views.reservations, name='reservations')
 ]
